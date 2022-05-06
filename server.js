4a63730bbc52c53ac458e27692daa05ccdb77d4c
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-06 11:17:37 -0500

    routes setup

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const app = express();
 const path = require("path");
 const notes = require("./db/db.json");
 const fs = require("fs");
+const { runInNewContext } = require("vm");
 app.use(express.json());
-app.get("/api/notes", (req, res) => res.json(notes));
-app.post("/api/notes", (req, res) =>{
 
-} );
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname , "public" , "notes.html")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname , "public" , "index.html")));
+
+app.get("/api/notes", (req, res) => res.json(notes));
+app.post("/api/notes", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`API server now on port http://localhost${PORT}`);
