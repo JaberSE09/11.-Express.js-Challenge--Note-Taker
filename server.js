898c1c0c472ec41c39a33e6325a595f9c3f5b01e
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-07 15:14:47 -0500

    create new note setup

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ const app = express();
 const path = require("path");
 const notes = require("./db/db.json");
 const fs = require("fs");
-const { runInNewContext } = require("vm");
+const { createNewNote, validateNotes } = require("./lib/notes");
+const uuid = require('uuid');
 app.use(express.json());
+
 const notesHTML = path.join(__dirname, "public", "notes.html");
 const indexHTML = path.join(__dirname, "public", "index.html");
+
 app.get("/notes", (req, res) => res.sendFile(notesHTML));
 app.get("*", (req, res) => res.sendFile(indexHTML));
 
 app.get("/api/notes", (req, res) => res.json(notes));
-app.post("/api/notes", (req, res) => {});
+app.post("/api/notes", (req, res) => {
+  req.body.id = uuid.v4()
+
+  if (!validateNotes(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    let noteEl = createNewNote(req.body, notes);
+    res.json(noteEl);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port http://localhost${PORT}`);
