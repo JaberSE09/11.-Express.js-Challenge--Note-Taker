cffcdea22a06d9a00617510fd97399f9fd7d2ea3
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-07 16:26:23 -0500

    fixed the html file

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const path = require("path");
 const notes = require("./db/db.json");
 const fs = require("fs");
 const { createNewNote, validateNotes } = require("./lib/notes");
-const uuid = require('uuid');
+const uuid = require("uuid");
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
 app.use(express.json());
 
 const notesHTML = path.join(__dirname, "public", "notes.html");
@@ -13,10 +20,9 @@ const indexHTML = path.join(__dirname, "public", "index.html");
 
 app.get("/notes", (req, res) => res.sendFile(notesHTML));
 app.get("*", (req, res) => res.sendFile(indexHTML));
-
 app.get("/api/notes", (req, res) => res.json(notes));
 app.post("/api/notes", (req, res) => {
-  req.body.id = uuid.v4()
+  req.body.id = uuid.v4();
 
   if (!validateNotes(req.body)) {
     res.status(400).send("The note is not properly formatted.");
@@ -27,5 +33,5 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`API server now on port http://localhost${PORT}`);
+  console.log(`API server now on port http://localhost:${PORT}`);
 });
